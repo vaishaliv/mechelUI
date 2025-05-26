b4b38b4b51095b3078b19750e49e84c3b13a129b
Vaishali <vaishali.viragi@gmail.com>
2025-05-26 05:22:04 -0400

    oauth implementation -2

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-const CLIENT_ID = import.meta.env.VITE_CLIENT_ID
+const CLIENT_ID = '492964619158-5llt7b3osh2587rioil2k6dcts907dcj.apps.googleusercontent.com'
 
 createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={CLIENT_ID}>
